58bad1cc796118726cd3bf50217de3681abad7c4
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-09-01 08:49:43 +0530

    navbar

---
+++ src/app/(main)/_components/Navbar.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useUserData } from "@/context/UserDataProvider";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function Navbar(){
+    const {user, loading} = useUserData();
+    return (
+        <div className="bg-white py-5 px-4 flex ">
+
+            <div>
+                <SidebarTrigger/>
+            </div>
+        </div>
+    )
+}
